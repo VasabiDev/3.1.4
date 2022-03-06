778ed64769e914ca7f2d4016afad183c65217e65
Anastasia Konovalova <pochtaplay2013@gmail.com>
2022-03-07 01:13:09 +0300

    mapping fixes

--- src/main/java/com/example/demo/configs/MvcConfig.java
+++ src/main/java/com/example/demo/configs/MvcConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/admin/users").setViewName("users");
+        registry.addViewController("/users").setViewName("users");
+        registry.addViewController("/user").setViewName("user");
 
     }
 }
